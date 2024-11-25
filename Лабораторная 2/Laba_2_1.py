0d22d1cd66a42b50596f24b79fa02cca964f1870
Aboba914 <prokhorov.artjom2004@gmail.com>
2024-11-25 19:40:20 +0300

    Add files via upload

---
+++ Лабораторная 2/Laba_2_1.py
@@ -0,0 +1,14 @@
+money_capital = 20000  # Подушка безопасности
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+increase = 0.05  # Ежемесячный рост цен
+count_mounth = 0
+
+while spend <= money_capital + salary:
+    count_mounth += 1
+    money_capital -= spend-salary
+    spend *= 1 + increase
+
+# TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
+
+print("Количество месяцев, которое можно протянуть без долгов:", count_mounth)
